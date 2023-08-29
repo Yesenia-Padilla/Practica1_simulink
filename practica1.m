2de53bd64741ae50e753468057ca477e9aceb6ea
Yesenia Lilibeth Padilla González <143239476+Yesenia-Padilla@users.noreply.github.com>
2023-08-28 20:07:10 -0600

    Add files via upload

---
+++ practica1.m
@@ -0,0 +1,10 @@
+%----Se definen los valores de los parámetros de la tabla necesarios en la ecuación diferencial----%
+m1 = 0.2;
+l1 = 0.3;
+m2 = 0.1;
+l2 = 0.25;
+g = 9.81;
+c1= 0.2;
+
+%--Llama al archivo de simulink para enviar los valores de los parámetros--%
+sim("practica1_simulink")
